74732ca98ba12f69ffefedc51989c6b1fa582f3e
Shalini Kumari Rai <49594352+shalini8038@users.noreply.github.com>
2021-01-20 11:36:51 +0530

    Update db_helper.py

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,16 +1,14 @@
+import pandas as pd
+data = [['Kritika',20000], ['Harsh',25000], ['Ragini',10000],['Rohit',8000]]
+ds= pd.DataFrame(data,columns=['Name','Salary'])
+
 class DbHelper:
     def get_maximum_salary(self):
-        '''
-        Implement the logic to find and return maximum salary from employee table
-        '''
-        return None
+        return ds['Salary'].max()
 
     def get_minimum_salary(self):
-        '''
-        Implement the logic to find and return minimum salary from employee table
-        '''
-        return None
-
+        return ds['Salary'].min()
+        
 if __name__ == "__main__":
     db_helper = DbHelper()
     min_salary = db_helper.get_minimum_salary()
